3b4abb5ca67a5288165de1ffca18792b19f9645b
Tanvir Ahamed <ahamedtanvir374@gmail.com>
2025-03-22 18:04:31 +0600

    i

---
+++ Variable/variable.js
@@ -0,0 +1,69 @@
+const age = 23;
+console.log(age)
+const booksToRead = 5;
+console.log(booksToRead)
+const wakeUpTime = 8
+console.log(wakeUpTime)
+const wastedTime = 5
+console.log(wastedTime)
+const borrow = 0
+console.log(borrow)
+const goals = 3
+console.log(goals)
+const pillow = 3
+console.log(pillow)
+
+
+
+const name = "Tanvir Ahamed"
+const address = "Narayangonj, Dhaka"
+const  friendUniversity = "Dhaka University"
+const fan = "Katrina Kaif"
+const movieOfTheYear = "Puspa 2"
+const hobby = "Programming"
+const color = "Blue"
+
+
+const friendHappy = false
+const crush = false
+const celebrating = true
+const isHappy = true
+const isMoney = true
+const isHungry = true
+const isStudyDone = true
+const isDInnerDone = true
+
+
+
+const hasMoney = 100 - 40
+const usesPencil = 10 / 2
+const khoroj = 60 / 15
+const khorojBaki = 60 - 15
+const helloWorld = "Hello" + "World"
+const kg = 261 + 153
+const taka = 500 / 75
+const takaBaki = 500 - 75
+const vagses = 8 % 3
+const bangladesh = "Bangla" + "Desh"
+const chal = 480 / 4
+const chalMonth = 480 / 12
+
+
+
+const price = 555
+const learning = true
+const megaCIty = "Dhaka"
+const temperature = 25
+const isHyngry = true
+const isRich = true
+
+
+
+
+
+
+
+
+
+
+
