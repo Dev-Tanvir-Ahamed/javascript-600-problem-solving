ddea148edefd2e6dc82e28620a2e325fb2c4e2c7
Tanvir Ahamed <ahamedtanvir374@gmail.com>
2025-03-29 00:42:40 +0600

    add variable problems

--- Variable/variable.js
+++ Variable/variable.js
@@ -61,6 +61,118 @@ const isRich = true
 
 
 
+let age2 = 22
+age2 = 23
+console.log(age2)
+
+let pokentMoney = 500
+pokentMoney = 150
+console.log(pokentMoney)
+
+let studyTime = 4
+studyTime = 0
+console.log(studyTime)
+
+let friendsCount = 3
+friendsCount = 2
+console.log(friendsCount)
+
+let phonePrice = 25000
+phonePrice = 12000
+console.log(phonePrice)
+
+let chairsCount = 40
+chairsCount = 38
+chairsCount = 32
+console.log(chairsCount)
+
+let playTime = 3
+playTime = 0
+console.log(playTime)
+
+
+/* comment */
+
+// score in cricket
+const score = 100
+
+// radius of circle
+const radius = 10
+
+/*
+dress1 - panjabi
+dress2 = shirt
+dress3 = jeans
+*/
+const dress = 3
+
+const giftMoney = 100
+// for buy a t-shirt
+
+const studyTime2 = 7
+
+/*
+
+3h - English
+4h - Maths
+
+
+*/
+
+const sendMoney = 100
+
+// send  money 100, charge - 5
+
+// added product rate and delivery rate
+const pay = 125
+
+
+
+//? Reserved Keyword
+
+const meat = 9
+const city = "Dhaka"
+const City = "Chottrogram"
+console.log(city)
+console.log(City)
+
+const userName = "Johan"
+const username = "Jovan"
+console.log(userName)
+console.log(username)
+
+const homeAddress = "Narayangonj, Dhaka"
+console.log(homeAddress)
+
+const super = 15
+console.log(super)
+
+const class = 12
+console.log(class)
+
+const 2ndPositon = "Imrul Kayes"
+console.log(secondPositon)
+
+
+
+//? NaN
+
+const num1 = "20"
+const num2 = 10
+const sum = parseInt(num1) + parseInt(num2)
+console.log(sum)
+
+const pi = "3.1416"
+console.log(parseInt(pi));
+
+const str = "premikBoy"
+console.log(parseInt(str)); // NaN
+
+const sum2 = 3.456 + 2.789
+console.log(parseFloat(sum2.toFixed(2))); 
+
+
+
 
 
 
